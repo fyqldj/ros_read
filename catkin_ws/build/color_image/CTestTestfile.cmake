39dd064b354826341796369d59881df52f6ddfdf
fyqldj <137867514+fyqldj@users.noreply.github.com>
2023-07-05 03:36:21 +0000

    Delete catkin_ws directory

--- catkin_ws/build/color_image/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Project/catkin_ws/src/color_image
-# Build directory: /home/ubuntu/Project/catkin_ws/build/color_image
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/color_image/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ubuntu/Project/catkin_ws/src/color_image/include".split(';') if "/home/ubuntu/Project/catkin_ws/src/color_image/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "cv_bridge;roscpp;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "color_image"
-PROJECT_SPACE_DIR = "/home/ubuntu/Project/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
